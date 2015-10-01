cace5f40ce0229609832e03ed531dbb13a956a95
Anna <annaluiza.medeiros@yahoo.com.br>
2015-10-01 20:13:45 -0300

    Commit Inicial
    Customização em Add Product, novo campo Product Observations

---
+++ Presentation/Nop.Web/Administration/Validators/Catalog/ProductValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Nop.Admin.Models.Catalog;
+using Nop.Services.Localization;
+using Nop.Web.Framework;
+using Nop.Web.Framework.Validators;
+
+namespace Nop.Admin.Validators.Catalog
+{
+    public class ProductValidator : BaseNopValidator<ProductModel>
+    {
+        public ProductValidator(ILocalizationService localizationService)
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage(localizationService.GetResource("Admin.Catalog.Products.Fields.Name.Required"));
+            RuleFor(m => m.ProductObservation).Length(0, 255);
+
+        }
+        [NopResourceDisplayName("Admin.Catalog.Products.Fields.ProductObservation")]
+        public string ProductObservation { get; set; }
+        
+    }
+}
